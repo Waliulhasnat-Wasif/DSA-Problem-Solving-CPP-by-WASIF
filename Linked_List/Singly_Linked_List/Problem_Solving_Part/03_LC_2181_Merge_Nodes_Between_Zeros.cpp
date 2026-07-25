62bf283a0fa5f4131f1fa34ca9405037031f28ed
Waliulhasnat-Wasif <waliulhasnatwasif@gmail.com>
2026-07-25 15:37:23 +0600

    Implement in-place merging of nodes between zeros with strict memory cleanup

---
+++ Linked_List/Singly_Linked_List/Problem_Solving_Part/03_LC_2181_Merge_Nodes_Between_Zeros.cpp
@@ -0,0 +1,128 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using std ::cout;
+using std ::string;
+using std ::vector;
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+
+    ListNode() : val(0), next(nullptr) {}
+    explicit ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution
+{
+public:
+    ListNode *mergeNodes(ListNode *head)
+    {
+        ListNode *reader = head->next;
+        ListNode *writer = head;
+
+        while (reader != nullptr)
+        {
+            int sum = 0;
+            ListNode *sumNode = reader;
+
+            while (reader != nullptr && reader->val != 0)
+            {
+                sum += reader->val;
+                reader = reader->next;
+            }
+
+            sumNode->val = sum;
+            writer->next = sumNode;
+            writer = sumNode;
+
+            ListNode *toDelete = sumNode->next;
+            while (toDelete != reader)
+            {
+                ListNode *nextDel = toDelete->next;
+                delete toDelete;
+                toDelete = nextDel;
+            }
+
+            ListNode *zeroNode = reader;
+            if (reader != nullptr)
+                reader = reader->next;
+
+            delete zeroNode;
+        }
+
+        writer->next = nullptr;
+
+        ListNode *finalHead = head->next;
+        delete head;
+
+        return finalHead;
+    }
+};
+
+ListNode *createLinkedList(const vector<int> &nums)
+{
+    if (nums.empty())
+        return nullptr;
+
+    ListNode *head = new ListNode(nums[0]);
+    ListNode *curr = head;
+
+    for (size_t i = 1; i < nums.size(); i++)
+    {
+        curr->next = new ListNode(nums[i]);
+        curr = curr->next;
+    }
+
+    return head;
+}
+
+void printLinkedList(ListNode *head)
+{
+    while (head != nullptr)
+    {
+        cout << head->val << " -> ";
+        head = head->next;
+    }
+
+    cout << "NULL" << "\n";
+}
+
+void deleteLinkedList(ListNode *head)
+{
+    while (head != nullptr)
+    {
+        ListNode *temp = head;
+        head = head->next;
+        delete temp;
+    }
+}
+
+void runTestCase(const vector<int> &input, const string &testName)
+{
+    Solution sol;
+
+    cout << "Test Case: " << testName << "\n";
+    ListNode *head = createLinkedList(input);
+    cout << "Original List: ";
+    printLinkedList(head);
+
+    ListNode *resultHead = sol.mergeNodes(head);
+    cout << "Modified List: ";
+    printLinkedList(resultHead);
+    cout << string(40, '-') << "\n";
+
+    deleteLinkedList(resultHead);
+}
+
+int main()
+{
+    cout << "--- Testing LeetCode 2181: Merge Nodes in Between Zeros ---\n\n";
+
+    runTestCase({0, 1, 2, 0, 4, 5, 6, 0, 8, 9, 0}, "Test 01: Standard");
+    runTestCase({0, 1, 2, 3, 0}, "Test 02: Standard");
+    runTestCase({0, 1, 2, 3, 0, 5, 0, 6, 0, 2, 6, 8, 0}, "Test 03: Multiple & Single Values");
+}
